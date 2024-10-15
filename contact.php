6ccb456b98d96b10253eb8dbbeb2e818baad4517
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-10-15 20:01:12 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,12 +8,31 @@
 </head>
 
 <body>
-    <?php require_once("nav.php")
+    <?php require_once("nav.php");
+    try {
+        $baseDeDonnee3 = new PDO('mysql:host=localhost;port=3307;dbname=automobile;charset=utf8', 'root', "root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    // $updatesq5 = $baseDeDonnee3->prepare('SELECT departement_nom from departement WHERE departement_code > 40');
+    $updatesq5 = $baseDeDonnee3->
+        prepare('SELECT departement.departement_nom bolide.email from bolide INNERT JOIN bolide ON departement.id = bolide.id');
+
+    $updatesq5->execute();
+
+    $final = $updatesq5->fetchAll();
+
+    foreach ($final as $key) {
+        // echo $key["departement_nom"];
+        echo $key;
+    }
 
 
 
 
-        ?>
+    ?>
 
 </body>
 
