5a6357c81912150719957380e12f0a9eef13656c
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-10-17 23:04:05 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 <body>
     <?php require_once("nav.php");
     try {
-        $baseDeDonnee3 = new PDO('mysql:host=localhost;port=3307;dbname=automobile;charset=utf8', 'root', "root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $baseDeDonnee3 = new PDO('mysql:host=localhost;port=3307;dbname=projet;charset=utf8', 'root', "root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
@@ -19,12 +19,20 @@
     // $updatesq5 = $baseDeDonnee3->prepare('SELECT departement_nom from departement WHERE departement_code > 40');
     
     // jointure
+    // $updatesq5 = $baseDeDonnee3->
+    //     prepare('SELECT email, Modification ,departement_nom
+    //     FROM bolide
+    //     INNER JOIN departement
+    //     bolide_id ON departement_id 
+    //     limit 1
+    //     ');
+    
     $updatesq5 = $baseDeDonnee3->
-        prepare('SELECT email, Modification ,departement_nom
-        FROM bolide
-        INNER JOIN departement
-        bolide_id ON departement_id 
-        limit 2
+        prepare('SELECT commentaire1, prenom 
+        FROM commentaires
+        INNER JOIN utilisateurs
+        com_id ON ut_id 
+ 
         ');
 
     $updatesq5->execute();
@@ -33,7 +41,8 @@
 
     foreach ($final as $key) {
         // echo $key["departement_nom"];
-        echo $key;
+        echo $key["commentaire1"];
+        echo $key["prenom"];
     }
 
 
