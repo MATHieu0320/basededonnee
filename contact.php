4cb485dcb0fb75522438e84cd4cdc0b28ac7f735
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-10-19 19:53:50 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,53 +5,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php require_once("nav.php");
-    try {
-        $baseDeDonnee3 = new PDO('mysql:host=localhost;port=3307;dbname=projet;charset=utf8', 'root', "root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    <!-- <?php require_once("nav.php"); ?> -->
+    <?php require_once("home.php"); ?>
+    <?php require_once("Contact.php"); ?>
+    <?php $dataPost444 = $_POST;
 
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
-
-    // $updatesq5 = $baseDeDonnee3->prepare('SELECT departement_nom from departement WHERE departement_code > 40');
-    
-    // jointure
-    // $updatesq5 = $baseDeDonnee3->
-    //     prepare('SELECT email, Modification ,departement_nom
-    //     FROM bolide
-    //     INNER JOIN departement
-    //     bolide_id ON departement_id 
-    //     limit 1
-    //     ');
-    
-    $updatesq5 = $baseDeDonnee3->
-        prepare('SELECT commentaire1, prenom 
-        FROM commentaires
-        INNER JOIN utilisateurs
-        com_id ON ut_id 
- 
-        ');
-
-    $updatesq5->execute();
-
-    $final = $updatesq5->fetchAll();
-
-    foreach ($final as $key) {
-        // echo $key["departement_nom"];
-        echo $key["commentaire1"];
-        echo $key["prenom"];
+    if (empty($dataPost444["area"])) {
+        $dataPost444["area"] = "aucun";
     }
 
 
+
+    $sqllll = 'INSERT INTO commentaires(commentaire1,commentaire2,commentaire3) VALUES (:commentaire1,:commentaire2,:commentaire3)';
+    $creer = $baseDeDonnee3->prepare($sqllll);
+
+
+
+
+    $creer->execute(
+        [
+            'commentaire1' => $dataPost444["area"],
+            'commentaire2' => $dataPost444["area"],
+            'commentaire3' => $dataPost444["area"]
+        ]
+    );
 
 
     ?>
-    <pre>
- <?php print_r($final) ?>    </pre>
-    </pre>
+
+    <?php if (isset($_SESSION["connexion"])): ?>
+        <form action="modification.php" method="POST">
+            <div>
+
+            </div>
+            <div>
+                <input type="text">
+            </div>
+            <div>
+                <h4>comments</h4>
+                <textarea name="area" height="200px" width="200px" id="area"></textarea>
+            </div>
+            <input type="submit">
+        </form>
+    <?php endif; ?>
+
+
 </body>
 
 </html>
